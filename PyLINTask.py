35ee8abaa570ad579e8c13118524ccf6a0112ecf
AasaiAlangaram <43054456+AasaiAlangaram@users.noreply.github.com>
2019-09-30 17:49:23 +0900

    Update PyLINTask.py

--- PyLINTask.py
+++ PyLINTask.py
@@ -1,11 +1,11 @@
 import PyLinDriver
 import time
 
-MasterID = 0x3C
-SlaveID = 0x7D
+MasterID = 0x00 #ECU Master ID
+SlaveID = 0x00 #Slave ID
 
-#Read output electric current 1 request frame
-SendFrame =[0x01,0x03,0x22,0x02,0x47,0xff,0xff,0xff]
+#Send Frame with Request msg
+SendFrame =[0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
 
 counter =0
 
@@ -17,7 +17,4 @@ while(1):
     x = PyLinDriver.SendHeaderFrame(SlaveID)
     print(x)
 
-#check sum calculation
-#The checksums are computed according to the following formula:
 
-#Checksum = INV (data byte 1 ⊕ data byte 2 ⊕ ... ⊕ data byte 8)    
